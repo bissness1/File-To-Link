98d410cce996484239d700d533d91de0def04561
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-12-18 23:32:33 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/meharfaizan/Uploadbot
-cd /Uploadbot
+git clone https://github.com/meharfaizan/File-To-Link
+cd /File-To-Link
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python -m Adarsh
