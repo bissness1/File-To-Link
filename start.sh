aff2846c0839a60ef0f3b05a6e14851ae42d069d
bissness1 <107295935+bissness1@users.noreply.github.com>
2022-12-19 13:04:53 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/meharfaizan/File-To-Link
+git clone https://github.com/bissness1/File-To-Link
 cd /File-To-Link
 pip3 install -r requirements.txt
 echo "Starting Bot...."
